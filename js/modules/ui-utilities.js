71f9e66ee032cca9bd916ad01c7fbf887672afcb
RahatDev11 <148705683+RahatDev11@users.noreply.github.com>
2025-10-13 23:27:01 +0600

    Add files via upload

--- js/modules/ui-utilities.js
+++ js/modules/ui-utilities.js
@@ -22,16 +22,27 @@ function openSidebar() {
     hideSocialMediaIcons();
     const sidebarOverlay = document.getElementById('sidebarOverlay');
     const sidebar = document.getElementById('sidebar');
-    if (sidebarOverlay) sidebarOverlay.classList.remove('hidden');
+    if (sidebarOverlay) {
+        sidebarOverlay.classList.remove('hidden');
+        setTimeout(() => {
+            sidebarOverlay.classList.add('opacity-100');
+        }, 10);
+    }
     if (sidebar) sidebar.classList.remove('-translate-x-full');
 }
 
 function closeSidebar() {
     const sidebarOverlay = document.getElementById('sidebarOverlay');
     const sidebar = document.getElementById('sidebar');
-    if (sidebarOverlay) sidebarOverlay.classList.add('hidden');
-    if (sidebar) sidebar.classList.add('-translate-x-full');
-}
+    if (sidebarOverlay) {
+        sidebarOverlay.classList.remove('opacity-100');
+        sidebar.classList.add('-translate-x-full');
+        setTimeout(() => {
+            sidebarOverlay.classList.add('hidden');
+        }, 300);
+    } else {
+        if (sidebar) sidebar.classList.add('-translate-x-full');
+    }
 function toggleSubMenuMobile(event) {
     event.stopPropagation();
     document.getElementById('subMenuMobile')?.classList.toggle('hidden');
